c8bf44fd1f3716e4c0bc6f4fa7c30301f9da0c81
naijaping <abbeyprince02@gmail.com>
2013-09-05 10:32:23 +0100

    Update bp-facebook-style-friend-list.php

--- bp-facebook-style-friend-list.php
+++ bp-facebook-style-friend-list.php
@@ -37,7 +37,74 @@ class Bp_Facebook_Style_Friend_List_Widget extends WP_Widget {
 	
 	function bp_facebook_style_friend_list_widget() {
 		$widget_ops = array('classname' => 'widget_facebook_style_friend_list','description' => __( "Show Photos And Names of User's Friends Or Current Viewing Member's Friends", "bp-facebook-style-friend-list") );
-		parent::WP_Widget( false, $name = __('FB Style Friend List','bp-facebook-style-friend-list'), $widget_ops);
+		parent::WP_Widget( false, $name = __('Friends','bp-facebook-style-friend-list'), $widget_ops);
 	}
 
+    //display
+    
+    function widget($args, $instance) {
+        global $bp;
+        //do not display if the user is not logged in or you are not viewing a profile
+        $user_id=false;
+        if(is_user_logged_in())
+            $user_id=bp_loggedin_user_id ();
+        
+        if(bp_is_user())
+            $user_id=bp_displayed_user_id ();
+        if(!$user_id)
+            return;
+                
+                extract( $args );
+                echo $before_widget;
+		echo $before_title
+		   . $widget_name
+		   . $after_title; ?>
+        
+        
+        <?php if ( bp_has_members( 'type=newest&max='. $instance['max_num'] . '&user_id='.bp_displayed_user_id() ) & is_user_logged_in() ) : ?>
 
+	 <ul id="members-list" class="item-list">
+	 <?php while ( bp_members() ) : bp_the_member(); ?>
+         <li>
+         <div class="item-avatar">
+         <a href="<?php bp_member_permalink() ?>"><?php bp_member_avatar('type=full&width=50&height=50') ?></a></div>
+         <div class="item">
+         <div class="item-title">
+	 <a style="color:#3B5998;"href="<?php bp_member_permalink() ?>"><?php bp_member_name() ?></a>
+         
+        <div class="clear"></div></div>
+	<?php endwhile; ?>
+         </div>
+		
+	<?php else: ?>
+
+
+			<div class="widget-error">
+				
+                                <p><?php _e( "Sorry, no friends found.", 'bp-facebook-style-friend-list' ); ?></p>
+			</div>
+
+		<?php endif; ?>
+
+		<?php echo $after_widget; ?>
+<?php
+	}
+
+function update( $new_instance, $old_instance ) {
+		$instance = $old_instance;
+		$instance['max_num'] = strip_tags( $new_instance['max_num'] );
+
+		return $instance;
+	}
+
+	function form( $instance ) {
+		$instance = wp_parse_args( (array) $instance, array( 'max_num' => 5 ) );
+		$max_num = strip_tags( $instance['max_num'] );
+		?>
+		
+		<p><label for="bp-facebook-style-friend-list-max-num"><?php _e( 'Max Number of Members:','bp-facebook-style-friend-list' ); ?> <input class="widefat" id="<?php echo $this->get_field_id( 'max_num' ); ?>" name="<?php echo $this->get_field_name( 'max_num' ); ?>" type="text" value="<?php echo attribute_escape( $max_num ); ?>" style="width: 30%" /></label></p>
+		
+	<?php
+	}
+}
+?>
